dd845bc774360864f4c73a94224912dd6f78a1be
ahmadnaseer143 <anaseer942@gmail.com>
2023-11-25 19:15:22 +0500

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/pages/Singin.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Singin = () => {
-  return <div>Singin</div>;
-};
-
-export default Singin;
