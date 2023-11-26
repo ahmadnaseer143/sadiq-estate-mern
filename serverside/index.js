641d072abeface96da44d1d5276dca8e89db52cc
ahmadnaseer143 <anaseer942@gmail.com>
2023-11-26 19:40:47 +0500

    Database connected

--- serverside/index.js
+++ serverside/index.js
@@ -1,6 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
