9b31bffd5ac6a4e347b499e43c2f6d6c5076cea2
ahmadnaseer143 <anaseer942@gmail.com>
2023-12-01 15:29:35 +0500

    Signin ui created

--- clientside/src/pages/Signin.jsx
+++ clientside/src/pages/Signin.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className="text-center text-3xl font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Enter Your Username"
+          className="border rounded-lg p-3 focus:outline-none"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          className="border rounded-lg p-3 focus:outline-none"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Enter Your Password"
+          className="border rounded-lg p-3 focus:outline-none"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white mt-4 p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-80">
+          Sign up
+        </button>
+      </form>
+      <div className="flex mt-5 gap-2">
+        <p>Have an account?</p>
+        <Link to={"/signin"}>
+          <span className="text-blue-700 hover:underline">Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
